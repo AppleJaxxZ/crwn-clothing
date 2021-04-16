76a1a4dc5e8cec65c6174e98ffc155ad5eb9c705
AppleJaxxZ <65720484+AppleJaxxZ@users.noreply.github.com>
2021-04-16 12:49:01 -0400

    Added Routing, update menu-items to nagivage with router

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import './homepage.styles.scss';
+
+
 import Directory from '../../components/directory/directory.components';
 const HomePage = () => (
     <div className='homepage'>
