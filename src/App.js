a5843fd4f744b49891a3f910fd8859099e1bfeb7
AppleJaxxZ <smackanalex@gmail.com>
2021-05-04 22:23:54 -0400

    Added redux, middleware, and added state to header componenet

--- src/App.js
+++ src/App.js
@@ -1,67 +1,56 @@
-import React from 'react';
-import './App.css';
-import {Switch, Route} from 'react-router-dom';
-import HomePage from './pages/homepage/homepage.component.jsx'
-import ShopPage from './components/shop/shop.component';
-import Header from './components/header/header.component.jsx';
-import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx';
-import { auth, createUserProfileDocument } from './firebase/firebase.utils';
+import React from "react";
+import "./App.css";
+import { Switch, Route } from "react-router-dom";
+import HomePage from "./pages/homepage/homepage.component.jsx";
+import ShopPage from "./components/shop/shop.component";
+import Header from "./components/header/header.component.jsx";
+import SignInAndSignUp from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx";
+import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 
-import { setCurrentUser } from './redux/user/user.actions';
+import { setCurrentUser } from "./redux/user/user.actions";
 
-import { connect } from 'react-redux';
+import { connect } from "react-redux";
 
 class App extends React.Component {
-  constructor() {
-    super()
-
-    this.state = {
-      currentUser: null
-    }
-  }
-
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+    const { setCurrentUser } = this.props;
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
       if (userAuth) {
         const userRef = await createUserProfileDocument(userAuth);
 
-        userRef.onSnapshot(snapShot => {
-          this.setState({
-            currentUser: {
-              id: snapShot.id,
-              ...snapShot.data()
-            }
-          })
-          console.log(this.state)
+        userRef.onSnapshot((snapShot) => {
+          setCurrentUser({
+            id: snapShot.id,
+            ...snapShot.data(),
+          });
         });
-        
       } else {
-        this.setState({currentUser: userAuth});
+        this.setState({ currentUser: userAuth });
       }
-    })  
-  };
-  componentWillUnmount(){
+    });
+  }
+  componentWillUnmount() {
     this.unsubscribeFromAuth();
   }
 
   render() {
-  return ( 
-    <div className="App">
-      <Header />
-      <Switch>
-        <Route exact path='/' component={HomePage} />
-        <Route path='/shop' component={ShopPage} />
-        <Route path='/signin' component={SignInAndSignUp} />
-
-      </Switch>
-    </div>
-  );
+    return (
+      <div className="App">
+        <Header />
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/shop" component={ShopPage} />
+          <Route path="/signin" component={SignInAndSignUp} />
+        </Switch>
+      </div>
+    );
+  }
 }
-}
-const mapDispatchToProps = dispatch => ({
 
-})
-// null because we dont need any stateTopProps from our reducer.  
-export default connect(null,)(App);
+const mapDispatchToProps = (dispatch) => ({
+  setCurrentUser: (user) => dispatch(setCurrentUser(user)),
+});
+// null because we dont need any stateTopProps from our reducer. connect is connecting the app with our dispatch
+export default connect(null, mapDispatchToProps)(App);
