a290938dea1cc415de445c68ea1f1bf6ff234c20
AppleJaxxZ <smackanalex@gmail.com>
2021-06-03 13:31:15 -0400

    Updated react-redux middleware logger

--- src/redux/store.js
+++ src/redux/store.js
@@ -9,7 +9,12 @@ import { logger } from "redux-logger";
 //root reducer is the main project reducer where all of the projects reducers flow into.
 import rootReducer from "./root-reducer";
 //assinged logger value to middleware array incase we want to add more items here later.
-const middlewares = [logger];
+//Later assigned middlewares to an empty array so we dont log each redux action in production builds.
+const middlewares = [];
+// this code says if process.env.Node_ENV is in development mode, then log the actions to the console.
+if (process.env.NODE_ENV === "development") {
+  middlewares.push(logger);
+}
 //creates your store, add in the rootReducer and the middleWare that calls all of the middleWare tools. {export store once persistor is added, although we technically dont need to export it.}
 const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
